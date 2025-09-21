4a8a6810c37ef76110f64de2f25d38160e6dd3b8
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2025-09-21 17:41:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { DataBase } from './database.js';
 
 // IMPORTANT: Replace with your actual Google Apps Script Web App URL.
-const API_URL = 'https://script.google.com/macros/s/AKfycbx4BoJu2CEBTllOBsWisVdtTzwCalZW7P_PeBWSiSfnKFoa9mrvOM2J77qM2wH-KqdhQw/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzCJ0tv-znJYr7acz1E1QpIM4G0o3OAu7tJzj4vJou1vkZ_Z2ajRjNbUReeyhe43zLGHA/exec';
 const db = new DataBase(API_URL);
 
 // NEW: Auth elements
