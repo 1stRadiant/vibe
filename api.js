da028b7483ebbc8a52a6ccb0b3ae7f8c7dccbe3d
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2025-09-25 22:22:44 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // api.js
 
 // IMPORTANT: Replace this with the Web App URL you got from deploying your Google Apps Script.
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0tsuBq7W4EldesbWSIjewatywE0vIwZaIaOfTy9kOfYXRjo_PpG2kZq9Hi7WM35QjNg/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby7YKHxsMVZbOEpaqa-MNf01C6WFJRDCYWZ2SZaLSAWb8awRlku5Wtl-YEMPC383ICU5w/exec';
 
 /**
  * Performs a JSONP request to the Google Apps Script backend.
